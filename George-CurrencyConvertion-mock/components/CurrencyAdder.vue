cce7d982726cf1af7bab5d48b569a57095776dd5
Antonius George S <atn010g@gmail.com>
2020-05-04 16:15:03 +0700

    chore row component: improve styling

---
+++ George-CurrencyConvertion-mock/components/CurrencyAdder.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="container">
+    <div v-if="isSelectMode == false">
+      <button @click="openChoice()" class="OpenSelect">
+        Add More
+      </button>
+    </div>
+    <div v-if="isSelectMode == true">
+      <div class="dropdown">
+        <button class="dropbtn">
+          {{ this.selectedChoice }} - {{ this.selectedCurrencyName }}
+        </button>
+        <div class="dropdown-content">
+          <div v-for="(value, index) of choiceList" :key="index">
+            <button @click="setChoice(value)" class="dropdown-content-style">
+              {{ value }}
+            </button>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <button @click="addDisplayCurrency()" class="OpenSelect">
+        ADD
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import cc from "currency-codes";
+export default {
+  data() {
+    return {
+      isSelectMode: false,
+      selectedChoice: "IDR",
+      selectedCurrencyName: "Rupiah",
+      displayCurrencyList: ["USD", "IDR"],
+      choiceList: ["JPY", "EUR", "HKD"]
+    };
+  },
+  methods: {
+    addDisplayCurrency() {
+      this.displayCurrencyList.push(this.selectedChoice);
+      this.isSelectMode = false;
+    },
+    setChoice(data) {
+      this.selectedChoice = data;
+      this.selectedCurrencyName = cc.code(data)["currency"];
+    },
+    openChoice() {
+      let data = this.choiceList[0];
+      this.selectedChoice = data;
+      this.selectedCurrencyName = cc.code(data)["currency"];
+      this.isSelectMode = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  display: block;
+  align-items: center;
+  text-align: center;
+  justify-content: space-evenly;
+}
+
+.seperator {
+  width: 32px;
+}
+
+.arrow {
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+}
+
+.down {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
+
+.OpenSelect {
+  text-align: center;
+  background-color: orange;
+  color: white;
+  display: inline-block;
+  width: 90%;
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+
+.dropbtn {
+  text-align: left;
+  cursor: pointer;
+  display: inline-block;
+  width: 100%;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+  width: 90%;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content-style {
+  text-align: left;
+  display: inline-block;
+  width: 100%;
+}
+
+.dropdown-content {
+  display: none;
+  text-align: left;
+  position: absolute;
+  background-color: #f9f9f9;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  width: 100%;
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  text-align: left;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {
+  background-color: #3e8e41;
+  color: white;
+}
+</style>
--- George-CurrencyConvertion-mock/components/Row.vue
+++ George-CurrencyConvertion-mock/components/Row.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="rowContained">
-    <div class="leftBox">
-      {{ currency }}
-      <br>
-      1 {{ current }} = {{ currency }} {{ rate }}
+    <div class="container">
+      <div class="rowContained">
+        <div class="container">
+          <div class="topLeft">
+            <h4>{{ currency }}</h4>
+          </div>
+          <div class="topRight">
+            <h4>{{ rate * amount }}</h4>
+          </div>
+        </div>
+        {{ currency }} - {{ currencyName }}
+        <br>
+        1 {{ current }} = {{ currency }} {{ rate }}
+      </div>
+      <div class="removeButton">
+        <button @click="removeCurrency">
+          X
+        </button>
+      </div>
     </div>
-    <div class="removeButton">
-      <button @click="removeCurrency">
-        X
-      </button>
-    </div>
-    <div class="rightBox">
-      {{ rate * amount }}
-    </div>
-    <br>
   </div>
 </template>
 
 <script>
+import cc from "currency-codes";
 export default {
   props: {
     current: {
       type: String,
-      default: 'USD'
+      default: "USD"
     },
     currency: {
       type: String,
-      default: 'USD'
+      default: "USD"
     },
     rate: {
       type: Number,
@@ -37,29 +44,52 @@ export default {
       default: 1
     }
   },
+  data() {
+    return {
+      currencyName: cc.code(this.currency)["currency"]
+    };
+  },
   methods: {
-    removeCurrency () {
-      this.$emit('emitRemoval', this.currency)
+    removeCurrency() {
+      this.$emit("emitRemoval", this.currency);
     }
   }
-}
+};
 </script>
 
 <style lang="css" scoped>
-.rowContained {
-    width: 100%;
-    display: flexbox;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+.container {
+  width: 100%;
+  display: flex;
+  align-items: left;
+  text-align: left;
 }
+
+.rowContained {
+  width: 100%;
+  display: flexbox;
+}
+
+.topLeft {
+  width: 100%;
+  display: flexbox;
+  align-items: left;
+  text-align: left;
+}
+
+.topRight {
+  width: 100%;
+  align-items: right;
+  text-align: right;
+}
+
 .innerRowContainer {
   overflow: hidden;
   width: 2000px;
 }
 
 .removeButton {
-  float:right;
+  float: right;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -67,15 +97,15 @@ export default {
   padding: 4pt;
 }
 .leftBox {
-    float: left;
-    align-items: flex-start;
-    justify-content: left;
-    text-align: left;
+  float: left;
+  align-items: flex-start;
+  justify-content: left;
+  text-align: left;
 }
 .rightBox {
-    float: right;
-    align-items: flex-end;
-    justify-content: right;
-    text-align: right;
+  float: right;
+  align-items: flex-end;
+  justify-content: right;
+  text-align: right;
 }
 </style>
